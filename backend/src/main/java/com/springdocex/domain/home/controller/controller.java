c1526070e2b87b67b925028f309a0fbb2953c89e
socra167 <socra167@gmail.com>
2025-02-12 11:44:45 +0900

    docs: 컨트롤러에 문서화 내용을 추가

--- backend/src/main/java/com/springdocex/domain/home/controller/controller.java
+++ backend/src/main/java/com/springdocex/domain/home/controller/controller.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "HomeController", description = "API 서버 홈")
 @Controller
 public class controller {
 
+	@Operation(summary = "API 서버 시작페이지", description = "API 서버 시작 페이지입니다.")
 	@GetMapping("/")
 	@ResponseBody
 	public String welcome() {
