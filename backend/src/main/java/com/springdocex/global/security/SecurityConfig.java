7e197c128aa78fe39db3bcf6b7598480761cc7bf
socra167 <socra167@gmail.com>
2025-02-12 11:20:08 +0900

    chore: spring security config에서 swagger 허용

--- backend/src/main/java/com/springdocex/global/security/SecurityConfig.java
+++ backend/src/main/java/com/springdocex/global/security/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig {
 					.permitAll()
 					.requestMatchers("/api/v1/posts/statistics")
 					.hasRole("ADMIN")
+					.requestMatchers("/","/swagger-ui/**", "/v3/api-docs/**")
+					.permitAll()
 					.anyRequest()
 					.authenticated()
 			)
