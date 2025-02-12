d6476d4b5f1aba7e26cae3861774983d35be211d
socra167 <socra167@gmail.com>
2025-02-12 12:43:49 +0900

    docs: SpringDocConfig에 인증 방법을 적용

--- backend/src/main/java/com/springdocex/global/springDoc/SpringDocConfig.java
+++ backend/src/main/java/com/springdocex/global/springDoc/SpringDocConfig.java
@@ -5,10 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "API 서버", version = "v1"))
+@SecurityScheme(
+	name = "Authorization",
+	type = SecuritySchemeType.HTTP,
+	scheme = "bearer",
+	bearerFormat = "JWT"
+)
 public class SpringDocConfig {
 
 	@Bean
