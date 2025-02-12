9e5f7042d0633906bb8dc412a21127fa281090e9
socra167 <socra167@gmail.com>
2025-02-12 10:37:41 +0900

    initial commit

---
+++ backend/src/main/java/com/springdocex/SpringDocExApplication.java
@@ -0,0 +1,13 @@
+package com.springdocex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocExApplication.class, args);
+	}
+
+}
